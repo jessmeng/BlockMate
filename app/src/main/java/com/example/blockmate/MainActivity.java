9c15dd92ea0b69892c6d139217812687d2dff1e9
Alvinosaur <ashek@andrew.cmu.edu>
2019-02-09 15:15:57 -0500

    functional backend

--- app/src/main/java/com/example/blockmate/MainActivity.java
+++ app/src/main/java/com/example/blockmate/MainActivity.java
@@ -6,16 +6,42 @@ import android.support.design.widget.BottomNavigationView;
 import android.os.Bundle;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.MenuItem;
+import android.content.Context;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+
 
 public class MainActivity extends AppCompatActivity {
 
     ActionBar toolbar;
+//    Context context;
+    private FirebaseOptions.Builder builder;
+    private FirebaseOptions options;
+
+    // public FirebaseDatabase dataBase;
+    // public DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        builder = new FirebaseOptions.Builder();
+        builder.setApiKey("AIzaSyDjiOg_OVDKPVF0i2x29f-ikpiljiH9Vf0");
+        builder.setApplicationId("blockmate-bdb3c");
+        builder.setDatabaseUrl("https://blockmate-bdb3c.firebaseio.com/");
+        FirebaseOptions options = builder.build();
+        FirebaseApp.initializeApp(this, options);
+
+        FirebaseDatabase dataBase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = dataBase.getReference("message");
+        myRef.setValue("Hello, World!");
+        Log.d("DEBUG", "onCreate: created msg");
 
         toolbar = getSupportActionBar();
         BottomNavigationView navigation = findViewById(R.id.navigationView);
