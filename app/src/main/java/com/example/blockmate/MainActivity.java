7acb3c0939a18510c9ae897e65e97dd8fa4798d5
Nekhera <32021246+Nekhera@users.noreply.github.com>
2019-02-09 00:01:59 -0500

    Home Screen

--- app/src/main/java/com/example/blockmate/MainActivity.java
+++ app/src/main/java/com/example/blockmate/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.blockmate;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.BottomNavigationView;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+        BottomNavigationView bottomNavigation = findViewById(R.id.navigationView);
     }
 }
